ea383b1bf35d74860568b98a66e0517704acf29f
Adrian Osmond <a.c.osmond@gmail.com>
2023-09-05 21:12:31 +0200

    Dependency updates

--- src/hooks/useVisualViewportHeight.ts
+++ src/hooks/useVisualViewportHeight.ts
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react';
 
 export default () => {
-  const [height, setHeight] = useState(`${visualViewport.height}px`);
+  const [height, setHeight] = useState(`${visualViewport?.height || 0}px`);
 
   useEffect(() => {
     const handler: () => void = () => {
-      setHeight(`${visualViewport.height}px`);
+      setHeight(`${visualViewport?.height || 0}px`);
     };
-    window.visualViewport.addEventListener('resize', handler);
-    return () => window.visualViewport.removeEventListener('resize', handler);
+    window.visualViewport?.addEventListener('resize', handler);
+    return () => window.visualViewport?.removeEventListener('resize', handler);
   }, []);
 
   return height;
